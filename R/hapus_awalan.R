52c66432e905ec52bdc6ab4d3c3d88d8839136fe
nurandi <nurandi.mail@gmail.com>
2015-12-06 18:16:58 +0700

    Remove function HapusAwalanDiKeSe

--- R/hapus_awalan.R
+++ R/hapus_awalan.R
@@ -2,16 +2,24 @@
 
 HapusAwalan <- function(kata) {
 
-  HapusAwalanDiKeSe(kata)
-  HapusAwalanBeTeMePe(kata)
+  if ( grepl("^di|[ks]e", kata) ) {
 
-}
+    kata1 <- HapusAwalanDiKeSe(kata)
+    if ( !is.null(kata1) ) {
+      return(kata1)
+    }
 
+    kata1 <- HapusAwalanDi2(kata) # diper-, dkk
+    if ( !is.null(kata1) ) {
+      return(kata1)
+    }
 
-## Remove prefix be-, te-, me-, pe-
-## eg. bertanya, terjadi, menjadi, pelajar
+    kata1 <- HapusAwalanKe2(kata) # keter-, dkk
+    if ( !is.null(kata1) ) {
+      return(kata1)
+    }
 
-HapusAwalanBeTeMePe <- function(kata) {
+  }
 
   if ( grepl("^[tmbp]e", kata) ) {
 
@@ -31,6 +39,16 @@ HapusAwalanBeTeMePe <- function(kata) {
     }
 
     kata1 <- HapusAwalanPe(kata)
+    if ( !is.null(kata1) ) {
+      return(kata1)
+    }
+
+    kata1 <- HapusAwalanMe2(kata) # memper-, menter-, dkk
+    if ( !is.null(kata1) ) {
+      return(kata1)
+    }
+
+    kata1 <- HapusAwalanBe2(kata) # berke-, dkk
     if ( !is.null(kata1) ) {
       return(kata1)
     }
