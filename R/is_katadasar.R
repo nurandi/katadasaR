fa0308c5ae4297305ebe48402d3fe4bfbf00b379
nurandi <nurandi.mail@gmail.com>
2015-12-06 16:58:32 +0700

    remove and add extra line

--- R/is_katadasar.R
+++ R/is_katadasar.R
@@ -4,15 +4,20 @@
 is_katadasar <- function(kata, kamus=NULL) {
 
   if ( is.null(kamus) ) {
+
     kata_dasar <- unique(kamus_katadasar)
+
   } else if ( is.vector(kamus) ) {
+
     kata_dasar <- unique(c(kamus_katadasar, kamus))
+
   } else {
+
     stop("Additional dictionary should be input vector!")
+
   }
 
   b <- !is.na(match(kata, kata_dasar))
 
   return(b)
-
 }
